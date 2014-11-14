87f2de1c80e19e51d694c9ddb3822c5e1214c406
mirage335 <spamfreemirage335@gmail.com>
2014-11-14 19:34:22 +0000

    first commit

---
+++ OpenEtherSurge.cmd
@@ -0,0 +1,122 @@
+# Pin name action command file
+
+# Start of element HUB2
+ChangePinName(HUB2, 3, 3)
+ChangePinName(HUB2, 2, 2)
+ChangePinName(HUB2, 1, 1)
+
+# Start of element HUB1
+ChangePinName(HUB1, 3, 3)
+ChangePinName(HUB1, 2, 2)
+ChangePinName(HUB1, 1, 1)
+
+# Start of element SHIELD
+ChangePinName(SHIELD, 1, 1)
+
+# Start of element STUB
+ChangePinName(STUB, 8, 8)
+ChangePinName(STUB, 7, 7)
+ChangePinName(STUB, 6, 6)
+ChangePinName(STUB, 5, 5)
+ChangePinName(STUB, 4, 4)
+ChangePinName(STUB, 3, 3)
+ChangePinName(STUB, 2, 2)
+ChangePinName(STUB, 1, 1)
+
+# Start of element L8
+ChangePinName(L8, 1, 1)
+ChangePinName(L8, 2, 2)
+
+# Start of element L7
+ChangePinName(L7, 1, 1)
+ChangePinName(L7, 2, 2)
+
+# Start of element L6
+ChangePinName(L6, 1, 1)
+ChangePinName(L6, 2, 2)
+
+# Start of element L5
+ChangePinName(L5, 1, 1)
+ChangePinName(L5, 2, 2)
+
+# Start of element L4
+ChangePinName(L4, 1, 1)
+ChangePinName(L4, 2, 2)
+
+# Start of element L3
+ChangePinName(L3, 1, 1)
+ChangePinName(L3, 2, 2)
+
+# Start of element L2
+ChangePinName(L2, 1, 1)
+ChangePinName(L2, 2, 2)
+
+# Start of element SPRK8
+ChangePinName(SPRK8, 2, 2)
+ChangePinName(SPRK8, 1, 1)
+
+# Start of element SPRK7
+ChangePinName(SPRK7, 2, 2)
+ChangePinName(SPRK7, 1, 1)
+
+# Start of element SPRK6
+ChangePinName(SPRK6, 2, 2)
+ChangePinName(SPRK6, 1, 1)
+
+# Start of element SPRK5
+ChangePinName(SPRK5, 2, 2)
+ChangePinName(SPRK5, 1, 1)
+
+# Start of element SPRK4
+ChangePinName(SPRK4, 2, 2)
+ChangePinName(SPRK4, 1, 1)
+
+# Start of element SPRK3
+ChangePinName(SPRK3, 2, 2)
+ChangePinName(SPRK3, 1, 1)
+
+# Start of element SPRK2
+ChangePinName(SPRK2, 2, 2)
+ChangePinName(SPRK2, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 6, GND)
+ChangePinName(U1, 7, in5)
+ChangePinName(U1, 8, in6)
+ChangePinName(U1, 9, in7)
+ChangePinName(U1, 10, in8)
+ChangePinName(U1, 5, Vmin)
+ChangePinName(U1, 4, in4)
+ChangePinName(U1, 3, in3)
+ChangePinName(U1, 2, in2)
+ChangePinName(U1, 1, in1)
+
+# Start of element SAFE
+ChangePinName(SAFE, 9, 9)
+ChangePinName(SAFE, 8, 8)
+ChangePinName(SAFE, 7, 7)
+ChangePinName(SAFE, 6, 6)
+ChangePinName(SAFE, 5, 5)
+ChangePinName(SAFE, 4, 4)
+ChangePinName(SAFE, 3, 3)
+ChangePinName(SAFE, 2, 2)
+ChangePinName(SAFE, 1, 1)
+
+# Start of element EXT
+ChangePinName(EXT, 9, 9)
+ChangePinName(EXT, 8, 8)
+ChangePinName(EXT, 7, 7)
+ChangePinName(EXT, 6, 6)
+ChangePinName(EXT, 5, 5)
+ChangePinName(EXT, 4, 4)
+ChangePinName(EXT, 3, 3)
+ChangePinName(EXT, 2, 2)
+ChangePinName(EXT, 1, 1)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element SPRK1
+ChangePinName(SPRK1, 2, 2)
+ChangePinName(SPRK1, 1, 1)
